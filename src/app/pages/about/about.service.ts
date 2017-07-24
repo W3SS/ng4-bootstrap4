583a2c769f5ae5cfd0423be2664406295ec6cd68
Mio86 <miodrag.maric86@gmail.com>
2017-07-24 14:10:55 +0200

    ready

--- src/app/pages/about/about.service.ts
+++ src/app/pages/about/about.service.ts
@@ -2,21 +2,21 @@ import { AboutMemeber } from './about.model';
 export class AboutService {
   private members: AboutMemeber[] = [
     new AboutMemeber(
-      '../../../assets/images/members/member1.jpg',
+      'assets/images/members/member1.jpg',
       'Lucian Lon',
       'Control Valve Assembly',
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.',
       'lucian@email.com'
     ),
     new AboutMemeber(
-      '../../../assets/images/members/member3.png',
+      'assets/images/members/member3.png',
       'Permelia Eddie',
       'Material Handling',
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.',
       'parmelia@email.com'
     ),
     new AboutMemeber(
-      '../../../assets/images/members/member2.jpg',
+      'assets/images/members/member2.jpg',
       'Leroi Martin',
       'Rotor Assembly',
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.',
